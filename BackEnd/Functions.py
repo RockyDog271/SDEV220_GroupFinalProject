dfb8db6cd69bc8a25d46dc72d895e3e327ec8daf
Logan Simila <Lsimila@ivytech.edu>
2026-04-19 05:12:57 -0400

    Drafted the CreateID function

--- BackEnd/Functions.py
+++ BackEnd/Functions.py
@@ -1,14 +1,47 @@
+from ast import While
+import string
+import random
 import json
 import os
 
 def AuthCheck(handler):
     pass
+#// RETURNS
+#“APPROVED” / “DENIED”
 
 def BackendAliveCheck(handler):
     pass
 
 def ListReminders(handler):
     pass
+#// RETURNS
+#Appointment .json(s)
+#// see Appointment_xxxxxxx.json for exact return
 
-def CreateID(IDrequest):
-    pass
+def CreateID():
+    # Getting the file path~
+    FilePath = os.path.join(os.getcwd(), "IDlist.txt")
+
+    # I don't remember what \/ does
+    if not os.path.exists(FilePath):
+        with open(FilePath, "w") as File:
+            pass
+    
+    while True:
+        # 3x random uppercase letters \/
+        Letters = "".join(random.choices(string.ascii_uppercase, k = 3))
+        # 4x random digits/numerals \/
+        Numerals = "".join(random.choices(string.digits, k = 4))
+        # Combining them \/
+        IDval = Letters + Numerals
+
+        with open(FilePath, "r") as File:
+            IDlist = {line.strip() for line in File if line.strip()}
+            # Thanks to my friend for writing /\, strings are hard >~<
+        if IDval not in IDlist:
+            with open(FilePath, "a") as File:
+                File.write(IDval + "\n")
+        return IDval
+ # Call function as follows;
+ # import Functions
+ # Var = Functions.CreateID()
